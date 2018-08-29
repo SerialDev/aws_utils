e72878f32f5b433b71be84bb203c45f0e674c3f3
Serialdev <carlos.mariscal.melgar@gmail.com>
2018-08-29 10:43:27 +0300

    [+] Structure definition aws_utils

---
+++ aws_utils/__init__.py
@@ -0,0 +1,1 @@
+from .ec2_utils import *
---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name = 'aws_utils',
+      version ='0.1',
+      description='Boto3 wrapper for convenience manipulation of aws',
+      url='',
+      author='Andres Mariscal',
+      author_email='carlos.mariscal.melgar@gmail.com',
+      license='MIT',
+      packages=['aws_utils'],
+      zip_safe=False)
+        
