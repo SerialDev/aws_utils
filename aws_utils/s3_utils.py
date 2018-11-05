5eea41b33aee698efc8dc193d6e11bbdc943931f
Serialdev <carlos.mariscal.melgar@gmail.com>
2018-11-05 15:21:24 +0200

    [+] Aws S3 support

--- aws_utils/s3_utils.py
+++ aws_utils/s3_utils.py
@@ -1,10 +1,13 @@
 
+import io
 import json
+import zlib
+
 import boto3
 import dill as pickle
-import zlib
-import io
-from core import *
+
+from .core import start_session
+
 
 class s3_utils(object):
     """
@@ -56,53 +59,54 @@ class s3_utils(object):
        Stores any iterable into s3 in different increments to allow for reconstruction/function application
 
     """
-    def __init__(self, key, secret):
+
+    def __init__(self, key, secret, region_name="eu-west-1"):
 
         self.key = key
         self.secret = secret
-        self.session = start_session(aws_access_key_id, aws_secret_access_key, region_name)
+        self.session = start_session(key, secret, region_name)
         self.client = self.session.client("s3")
         self.resource = self.session.resource("s3")
 
-
     def get_bucket(self, bucket_name):
-        self.current_bucket=  self.resource.Bucket(bucket_name)
+        self.current_bucket = self.resource.Bucket(bucket_name)
         return self.current_bucket
 
-
     def get_bucket_key(self, bucket_name, key_name):
-        self.current_bucket_key=  self.client().get_object(Bucket=bucket_name, Key=key_name)
+        self.current_bucket_key = self.client().get_object(
+            Bucket=bucket_name, Key=key_name
+        )
         return self.current_bucket_key
 
-
     def get_bucket_key_json(self):
-            if hasattr(self, 'current_bucket_key'):
-                for i in self.current_bucket_key['Body'].iter_lines():
-                    yield json.loads(i.decode())
-            else:
-                print("No current bucket object initialized")
-
+        if hasattr(self, "current_bucket_key"):
+            for i in self.current_bucket_key["Body"].iter_lines():
+                yield json.loads(i.decode())
+        else:
+            print("No current bucket object initialized")
 
     def get_bucket_info(self, prefix):
-        if hasattr(self, 'current_bucket'):
+        if hasattr(self, "current_bucket"):
             for obj in self.current_bucket.objects.filter(Prefix=prefix):
                 print(obj.key, obj.size)
-                o = obj
-            return o
+                yield obj
         else:
             print("Bucket has not yet been initialized")
 
-
     def to_s3(self, bucket_name, data, key_name):
-        response = self.client().put_object(Bucket=bucket_name,
-                                                   Body=zlib.compress(pickle.dumps(data)),
-                                                   Key=key_name)
+        response = self.client().put_object(
+            Bucket=bucket_name, Body=zlib.compress(pickle.dumps(data)), Key=key_name
+        )
         return response
 
-
     def from_s3(self, bucket_name, key_name):
-        return pickle.loads(zlib.decompress(self.client().get_object(Bucket=bucket_name, Key=key_name)['Body'].read()))
-
+        return pickle.loads(
+            zlib.decompress(
+                self.client()
+                .get_object(Bucket=bucket_name, Key=key_name)["Body"]
+                .read()
+            )
+        )
 
     def from_bin_file_streaming(self, name, bucket_name, key_name, full_path=False):
         if full_path:
@@ -110,20 +114,26 @@ class s3_utils(object):
         else:
             path = os.path.join(os.getcwd(), name)
 
-        with open(path, 'ab') as f:
-            obj = self.client().get_object(Bucket=bucket_name, Key=key_name)['Body'].iter_lines()
+        with open(path, "ab") as f:
+            obj = (
+                self.client()
+                .get_object(Bucket=bucket_name, Key=key_name)["Body"]
+                .iter_lines()
+            )
             for i in obj:
                 f.write(i)
 
-
     def from_bin_streaming(self, bucket_name, key_name):
         out_buffer = io.BytesIO()
-        obj = self.client().get_object(Bucket=bucket_name, Key=key_name)['Body'].iter_lines()
+        obj = (
+            self.client
+            .get_object(Bucket=bucket_name, Key=key_name)["Body"]
+            .iter_chunks()
+        )
         for i in obj:
             out_buffer.write(i)
         out_buffer.seek(0)
         return out_buffer
-
 
     def to_bin_streaming(self, data, bucket_name, key_name):
         out_buffer = io.BytesIO()
@@ -132,17 +142,15 @@ class s3_utils(object):
         result = self.client().upload_fileobj(out_buffer, bucket_name, key_name)
         return result
 
-
     def read_bin(self, name, full_path=False):
         if full_path:
             path = name
         else:
             path = os.path.join(os.getcwd(), name)
 
-        with open(path, 'rb') as f:
+        with open(path, "rb") as f:
             obj = f.read()
         return obj
-
 
     def iter_to_s3(self, bucket_name, iterable, key_name, increments=50):
 
@@ -150,7 +158,7 @@ class s3_utils(object):
         item_num = 0
         file_num = 0
         for item in iterable:
-            item_num +=1
+            item_num += 1
             print_iter(item_num)
             temp_list.append(item)
             if item_num != 0 and item_num % increments == 0:
@@ -161,14 +169,17 @@ class s3_utils(object):
         if temp_list != []:
             self.to_s3(bucket_name, temp_list, key_name + "_{}".format(file_num + 1))
 
-
     def iter_bucket(self, bucket_name):
         for i in self.resource.Bucket(bucket_name).objects.all():
             yield i
 
-
-    def get_s3_objects_containing(self, bucket_name, containing_string):
+    def get_s3_objects_containing(self, bucket_name, containing_string, gen=False):
         buckets = self.iter_bucket(bucket_name)
 
-        buckets = [i for i in buckets if containing_string in i.key]
-        return buckets
+        if gen:
+            for i in buckets:
+                if containing_string in i.key:
+                    yield i
+        else:
+            buckets = [i for i in buckets if containing_string in i.key]
+            return buckets
