6f00d703045cd62215787e5235a42ba940e8a6f6
Serialdev <carlos.mariscal.melgar@gmail.com>
2018-11-05 15:20:52 +0200

    [+] Core initialization

--- aws_utils/__init__.py
+++ aws_utils/__init__.py
@@ -1,1 +1,4 @@
+from .core import *
 from .ec2_utils import *
+from .s3_utils import *
+from .sqs_utils import *
--- aws_utils/core.py
+++ aws_utils/core.py
@@ -1,19 +1,13 @@
-import boto3
 import datetime
 
+import boto3
+
+
 def start_session(aws_access_key_id, aws_secret_access_key, region_name):
-    session = boto3.Session(region_name=region_name,
-                                     aws_access_key_id = self.key,
-                                     aws_secret_access_key = self.secret,
-                                     # aws_session_token=response['Credentials']['SessionToken']
+    session = boto3.Session(
+        region_name=region_name,
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        # aws_session_token=response['Credentials']['SessionToken']
     )
     return session
-
-
-
-
-
-
-
-
-
