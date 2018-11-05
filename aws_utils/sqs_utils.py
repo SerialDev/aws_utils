86ad1b84fb16546ced0b97ecb8fea23f610f55dc
Serialdev <carlos.mariscal.melgar@gmail.com>
2018-11-05 15:27:19 +0200

    [*] Linting with blacken

--- aws_utils/sqs_utils.py
+++ aws_utils/sqs_utils.py
@@ -16,134 +16,119 @@ class sqs_utils(object):
         self.current_id = self.session.client("sts").get_caller_identity()["Account"]
 
     def create_queue(self, queue_name, fifo_queue, delay_seconds=0,
-                     message_size=262_144, message_retention=345_600, content_deduplication=False, ):
+                     message_size=262144, message_retention=345600, content_deduplication=False, ):
         if fifo_queue == True:
-             attributes = {
-                 "DelaySeconds" : str(delay_seconds),
-                 "MaximumMessageSize": str(message_size),
-                 "MessageRetentionPeriod" : str(message_retention),
-                 "FifoQueue" : str(fifo_queue),
-                 "ContentBasedDeduplication" : str(content_deduplication),
-             }
+            attributes = {
+                "DelaySeconds": str(delay_seconds),
+                "MaximumMessageSize": str(message_size),
+                "MessageRetentionPeriod": str(message_retention),
+                "FifoQueue": str(fifo_queue),
+                "ContentBasedDeduplication": str(content_deduplication),
+            }
         else:
 
-             attributes = {
-                 "DelaySeconds" : str(delay_seconds),
-                 "MaximumMessageSize": str(message_size),
-                 "MessageRetentionPeriod" : str(message_retention),
-             }
+            attributes = {
+                "DelaySeconds": str(delay_seconds),
+                "MaximumMessageSize": str(message_size),
+                "MessageRetentionPeriod": str(message_retention),
+            }
 
-        response = self.client.create_queue( QueueName = queue_name, Attributes = attributes)
+        response = self.client.create_queue(QueueName=queue_name, Attributes=attributes)
         return response
-
 
     def delete_message(self, queue_url, receipt_handle):
-        response = self.client.delete_message( QueueUrl = queue_url, ReceiptHandle= receipt_handle)
+        response = self.client.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
         return response
-
 
     def gen_delete_entry(self, id, receipt_handle):
         return {"Id": id, "ReceiptHandle": receipt_handle}
 
-
     def delete_message_batch(self, queue_url, entry_list):
-        response = self.client.delete_message_batch(QueueUrl = queue_url, Entries = entry_list)
+        response = self.client.delete_message_batch(QueueUrl=queue_url, Entries=entry_list)
         return response
-
 
     def delete_queue(self, queue_url):
-        response = self.client.delete_queue(QueueUrl = queue_url)
+        response = self.client.delete_queue(QueueUrl=queue_url)
         return response
-
 
     def queue_attributes(self, queue_url, attributes_list):
-        response = self.client.get_queue_attributes(QueueUrl, AttributeNames = attributes_list)
+        response = self.client.get_queue_attributes(QueueUrl, AttributeNames=attributes_list)
         return response
-
 
     def queue_url(self, queue_name, queue_owner_id=None):
-        if queue_owner_id is None :
+        if queue_owner_id is None:
             queue_owner_id = self.current_id
-        response = self.client.get_queue_url(QueueName = queue_name, QueueOwnerAWSAccountId = queue_owner_id)
+        response = self.client.get_queue_url(QueueName=queue_name, QueueOwnerAWSAccountId=queue_owner_id)
         return response
-
 
     def list_queues(self, queue_name_prefix):
-        response = self.client.list_queues(QueueNamePrefix = queue_name_prefix)
+        response = self.client.list_queues(QueueNamePrefix=queue_name_prefix)
         return response
-
 
     def purge_queue(self, queue_url):
-        response = self.client.purge_queue(QueueUrl = queue_url)
+        response = self.client.purge_queue(QueueUrl=queue_url)
         return response
-
 
     def receive_message(self, queue_url, wait_time=0, attribute_name_list=['All'],
                         message_attribute_names=["All"], max_n_messages=1):
 
-        response = self.client.receive_message(QueueUrl = queue_url,
-                                               AttributeNames = attribute_name_list,
-                                               MessageAttributeNames = message_attribute_names,
-                                               WaitTimeSeconds = wait_time,
-                                               MaxNumberOfMessages = max_n_messages, )
+        response = self.client.receive_message(QueueUrl=queue_url,
+                                               AttributeNames=attribute_name_list,
+                                               MessageAttributeNames=message_attribute_names,
+                                               WaitTimeSeconds=wait_time,
+                                               MaxNumberOfMessages=max_n_messages, )
         return response
 
-
-    def send_message_fifo(self, queue_url, body, delay_seconds, dedup_id, group_id, attributes=None,  ):
+    def send_message_fifo(self, queue_url, body, delay_seconds, dedup_id, group_id, attributes=None,):
         if attributes is None:
-            response = self.client.send_message(QueueUrl = queue_url,
-                                            MessageBody = body,
-                                            DelaySeconds=delay_seconds,
-                                            MessageDeduplicationId = str(dedup_id),
-                                            MessageGroupId= group_id )
+            response = self.client.send_message(QueueUrl=queue_url,
+                                                MessageBody=body,
+                                                DelaySeconds=delay_seconds,
+                                                MessageDeduplicationId=str(dedup_id),
+                                                MessageGroupId=group_id)
 
         else:
-            response = self.client.send_message(QueueUrl = queue_url,
-                                                MessageBody = body,
+            response = self.client.send_message(QueueUrl=queue_url,
+                                                MessageBody=body,
                                                 DelaySeconds=delay_seconds,
-                                                MessageAttributes = attributes,
-                                                MessageDeduplicationId = str(dedup_id),
-                                                MessageGroupId= group_id )
+                                                MessageAttributes=attributes,
+                                                MessageDeduplicationId=str(dedup_id),
+                                                MessageGroupId=group_id)
 
         return response
-
 
     def send_message_standard(self, queue_url, body, delay_seconds, attributes=None, ):
         if attributes is None:
-            response = self.client.send_message(QueueUrl = queue_url,
-                                                MessageBody = body,
+            response = self.client.send_message(QueueUrl=queue_url,
+                                                MessageBody=body,
                                                 DelaySeconds=delay_seconds, )
 
         else:
-            response = self.client.send_message(QueueUrl = queue_url,
-                                                MessageBody = body,
+            response = self.client.send_message(QueueUrl=queue_url,
+                                                MessageBody=body,
                                                 DelaySeconds=delay_seconds,
-                                                MessageAttributes = attributes, )
-
+                                                MessageAttributes=attributes, )
 
         return response
 
-
     def gen_send_fifo_entry(self, queue_url, body, delay_seconds, attributes, dedup_id, group_id):
-        entry = {"QueueUrl" : queue_url,
-                   "DelaySeconds" : delay_seconds,
-                   "MessageAttributes" : attributes,
-                   "MessageDeduplicationId" : dedup_id,
-                   "MessageGroupId" : group_id }
+        entry = {"QueueUrl": queue_url,
+                 "DelaySeconds": delay_seconds,
+                 "MessageAttributes": attributes,
+                 "MessageDeduplicationId": dedup_id,
+                 "MessageGroupId": group_id}
         return entry
-
 
     def gen_send_std_entry(self, queue_url, body, delay_seconds, attributes,):
-        entry = {"QueueUrl" : queue_url,
-                   "DelaySeconds" : delay_seconds,
-                   "MessageAttributes" : attributes,
-                    }
+        entry = {"QueueUrl": queue_url,
+                 "DelaySeconds": delay_seconds,
+                 "MessageAttributes": attributes,
+                 }
         return entry
 
+    def send_message_batch(self, queue_url, entries):
 
-    def send_message_batch(self, queue_url, entries ):
-
-        response = self.client.send_message_batch(QueueUrl = queue_url,
-                                                  Entries= entries )
+        response = self.client.send_message_batch(QueueUrl=queue_url,
+                                                  Entries=entries)
 
         return response
